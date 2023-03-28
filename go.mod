265b463df32249a001f667aec9837b9e5becf39a
Ian Lopshire <ian@timehop.com>
2023-03-28 10:43:02 -0400

    Add lazy loading singleton implementation

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ianlopshire/go-singleton
 
 go 1.20
+
+require github.com/ianlopshire/go-async v1.0.0 // indirect
--- singleton.go
+++ singleton.go
@@ -32,3 +32,41 @@ func (s singleton[T]) Instance() (T, error) {
 func (s singleton[T]) InstanceCtx(ctx context.Context) (T, error) {
 	return s.instance, s.err
 }
+
+// Lazy is a thread-safe lazy-initialized singleton value.
+type lazy[T any] struct {
+	new      func() (T, error)
+	instance T
+	err      error
+
+	once  sync.Once
+	latch async.Latch
+}
+
+func NewLazy[T any](new func() (T, error)) Singleton[T] {
+	return &lazy[T]{new: new}
+}
+
+func (l *lazy[T]) InstanceCtx(ctx context.Context) (T, error) {
+	go func() {
+		l.once.Do(func() {
+			i, err := l.new()
+			async.Resolve(&l.latch, func() {
+				l.instance = i
+				l.err = err
+			})
+		})
+	}()
+
+	select {
+	case <-l.latch.Done():
+		return l.instance, l.err
+	case <-ctx.Done():
+		var zero T
+		return zero, ctx.Err()
+	}
+}
+
+func (l *lazy[T]) Instance() (T, error) {
+	return l.InstanceCtx(context.Background())
+}
